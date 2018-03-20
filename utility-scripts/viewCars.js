20b0fef7de1689fee30e5603c217d3e506a46f48
Chris <c.mcqueen@newcastle.ac.uk>
2018-03-20 14:06:40 +0000

    Added Query and Manufacture Car is working now

--- utility-scripts/viewCars.js
+++ utility-scripts/viewCars.js
@@ -1,19 +1,8 @@
 const bnUtil = require('./connection-util');
-bnUtil.connect(viewAllCars);
+bnUtil.connect(viewAllCars, 'NML@outbound-logistics');
 
 function viewAllCars() {
-    let bnDef = bnUtil.connection.getBusinessNetwork();
-    bnUtil.connection.getAssetRegistry('outbound.logistics.vehicle.Car')
-        .then((carRegistry) => {
-            return carRegistry.exists('VEHICLE1').then((exists) => {
-                console.log(exists);
-            });
-        }).then(() => {
-            bnUtil.connection.getTransactionRegistry('outbound.logistics.vehicle.ManufactureCar')
-            .then((manufactureRegistry) => {
-                manufactureRegistry.getAll().then((txs) => {
-                    console.log(txs);
-                })
-            })
-        })
+    return bnUtil.connection.query('AllCars').then((results) => {
+        console.log(results);
+    })
 }
