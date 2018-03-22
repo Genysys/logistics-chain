02e4dfc496af9d5029ae0c933a527df5968572cd
Chris <c.mcqueen@newcastle.ac.uk>
2018-03-22 15:07:54 +0000

    Added nissan nodes

--- utility-scripts/createRelevantNissanNodes.js
+++ utility-scripts/createRelevantNissanNodes.js
@@ -8,8 +8,8 @@ function createRelevantNissanNodes() {
     let bnDef = bnUtil.connection.getBusinessNetwork();
     let factory = bnDef.getFactory();
 
-
     vehicle.transportPlan.vehicleTransitNode.forEach(node => {
+      
         if (node.transitNode.transitNodeType.transitNodeTypeDesc === "Factory Compound" && node.transitNode.logisticsPartner.logisticsPartnerDesc === "NISSAN") {
 
             bnUtil.connection.getParticipantRegistry("outbound.logistics.participant.Compound")
@@ -22,7 +22,25 @@ function createRelevantNissanNodes() {
                     compoundRegistry.add(compound).then(function () {
                         console.log('Compound participant added: ' + node.transitNode.transitNode);
                         return bnUtil.provideIdentity('outbound.logistics.participant.Compound', node.transitNode.transitNode).then(function () {
-                            return bnUtil.disconnect();
+                            bnUtil.disconnect();
+                        })
+                    }).catch((error) => {
+                        console.log(error);
+                    })
+                });
+        }
+         if (node.transitNode.transitNodeType.transitNodeTypeDesc === "Port" && node.transitNode.logisticsPartner.logisticsPartnerCode === "NSA") {
+            bnUtil.connection.getParticipantRegistry("outbound.logistics.participant.Port")
+                .then((portRegistry) => {
+                    
+                    let port = factory.newResource('outbound.logistics.participant', 'Port', node.transitNode.logisticsPartner.logisticsPartnerCode);
+                    port.owner = factory.newRelationship('outbound.logistics.participant', "Manufacturer", vehicle.transportPlan.brand.brand);
+                    port.description = node.transitNode.logisticsPartner.logisticsPartnerDesc;
+
+                    portRegistry.add(port).then(function () {
+                        console.log('Port participant added: ' + node.transitNode.logisticsPartner.logisticsPartnerCode);
+                        return bnUtil.provideIdentity('outbound.logistics.participant.Port', node.transitNode.logisticsPartner.logisticsPartnerCode).then(function () {
+                            
                         })
                     }).catch((error) => {
                         console.log(error);
