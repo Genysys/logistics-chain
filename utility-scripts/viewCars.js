55a373856b9e4ecb1900cf7df4d5a100069d272f
Chris <c.mcqueen@newcastle.ac.uk>
2018-03-22 13:01:26 +0000

    added .gitignore

--- utility-scripts/viewCars.js
+++
@@ -1,9 +0,0 @@
-const bnUtil = require('./connection-util');
-bnUtil.connect(viewAllCars, 'NML@outbound-logistics');
-
-function viewAllCars() {
-    return bnUtil.connection.query('AllCars').then((results) => {
-        console.log(results);
-        bnUtil.disconnect();
-    })
-}
